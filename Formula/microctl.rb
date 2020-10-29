d0d929827d5acd8bb96c6b66553ea0e59ed27a57
Evgeni Sladkowski <kapodes@gmail.com>
2020-10-29 15:53:25 +0100

    Update microctl.rb
    release 0.3.0

--- Formula/microctl.rb
+++ Formula/microctl.rb
@@ -1,15 +1,15 @@
 class Microctl < Formula
   desc "Microconfig Server CLI"
   homepage "https://microconfig.io/"
-  version "0.2.0"
+  version "0.3.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/microconfig/microconfig-server/releases/download/v0.2.0/microctl-macos.zip"
-    sha256 "22b615b5b45d377197ca00a3ebd1280a9a27db49aa514c9bb16f8e7044264916"
+    url "https://github.com/microconfig/microconfig-server/releases/download/v0.3.0/microctl-macos.zip"
+    sha256 "b1668c7b90cc01c8f0de13589c3f5b4f96d9858cde155f26f6ee619563ee7a39"
   elsif OS.linux?
-    url "https://github.com/microconfig/microconfig-server/releases/download/v0.2.0/microctl-linux.zip"
-    sha256 "a1a9769fcbf5e796828f9405024b05b13eb324747ced21fe790e2b07a0d9c1d5"
+    url "https://github.com/microconfig/microconfig-server/releases/download/v0.3.0/microctl-linux.zip"
+    sha256 "efb394040a732ce9f50d4e4c62930fe0117719dc17877c574c83ac3937f91f78"
   end
 
   def install
